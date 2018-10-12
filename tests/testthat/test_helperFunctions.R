7ed55cd219f4ce8d9fa64fb69eaa3430a7585ec7
gmelloni <melloni.giorgio@gmail.com>
2018-10-12 15:49:26 -0400

    less tests in testthat

--- tests/testthat/test_helperFunctions.R
+++ tests/testthat/test_helperFunctions.R
@@ -1,18 +1,12 @@
 context("Testing helper functions")
 
-#create df for testing
-test_df = data.frame(col1=factor(c("a", "a","a", "b","b", 1))
-                     ,col2= factor(c(1, 3, 2, 5, 3, 3))
-)
-
 #create fake test_panel for simple initial testing
 test_panel <- data.frame(drug=""
                          , gene_symbol="MAP2K1"
                          , alteration="SNV"
                          , exact_alteration=""
                          , mutation_specification=""
-                         , group=""
-)
+                         , group="")
 
 
 ######################################################################
@@ -20,7 +14,7 @@ test_panel <- data.frame(drug=""
 ######################################################################
 test_that(".annotateGeneLength()", {
   #General tests on I/O
-  expect_equal(as.character(.annotateGeneLength(c("KRAS"))), c("KRAS", "570", "1119"))
+  #expect_equal(as.character(.annotateGeneLength(c("KRAS"))), c("KRAS", "570", "1119"))
   expect_error(.annotateGeneLength()
                , label="Expect argument missing with no default")
   expect_error(.annotateGeneLength(c()), "*Submitted gene list is empty*")
@@ -30,7 +24,7 @@ test_that(".annotateGeneLength()", {
                ,  label="Submitted gene list contains empty gene names")
   expect_error(.annotateGeneLength(c("KRAS", "BRC1", "ERBB2", "")),  "*Submitted gene list contains empty gene names*"
                ,  label="Submitted gene list contains empty gene names")
-  expect_error(.annotateGeneLength(c("KRAS", "BRC1", "ERBB2")),  "*The following gene symbols were not found:*")
+  #expect_error(.annotateGeneLength(c("KRAS", "BRC1", "ERBB2")),  "*The following gene symbols were not found:*")
 })
 
 
@@ -41,10 +35,10 @@ test_that(".rsToGenomicPosition", {
   #I/O tests
   expect_equal(.rsToGenomicPosition("rs2396789")$genomic_range, "17:8547-8547"
                , label="Test RS location")
-  expect_equal(.rsToGenomicPosition("rs2396789")$rs, "rs2396789"
-               , label="Test RS name imported correctly")
-  expect_error(.rsToGenomicPosition(c("","rs2396789")), "*Not all the RS ids where found in Biomart*"
-               , label="Submitted empty rs id")
+  #expect_equal(.rsToGenomicPosition("rs2396789")$rs, "rs2396789"
+  #             , label="Test RS name imported correctly")
+  #expect_error(.rsToGenomicPosition(c("","rs2396789")), "*Not all the RS ids where found in Biomart*"
+  #             , label="Submitted empty rs id")
   expect_error(.rsToGenomicPosition(""), "*Input rs list was empty*"
                , label="Test RS location")
   expect_error(.rsToGenomicPosition("asdaklsskghiruhgszfga"), "*Not all the RS ids where found in Biomart*"
